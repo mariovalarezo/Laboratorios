33331cb9c5ef76cf86f29d5edc8daec771d93e49
David Pardo <43886292+davidPardoC@users.noreply.github.com>
2020-06-03 18:50:44 -0500

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -10,7 +10,7 @@ CREATE TABLE paciente (
   telefono VARCHAR(45) NULL,
   direccion VARCHAR(45) NULL,
   PRIMARY KEY (cedula))
-ENGINE = InnoDB;
+
 
 
 CREATE TABLE medico (
@@ -20,14 +20,14 @@ CREATE TABLE medico (
   especialidad VARCHAR(45) NULL,
   telefono VARCHAR(45) NULL,
   PRIMARY KEY (cedula))
-ENGINE = InnoDB;
+
 
 
 CREATE TABLE medicamento (
   nombre VARCHAR(100) NOT NULL,
   tipo VARCHAR(45) NULL,
-  PRIMARY KEY (`nombre`))
-ENGINE = InnoDB;
+  PRIMARY KEY (nombre))
+
 
 
 CREATE TABLE tratamiento (
@@ -37,19 +37,13 @@ CREATE TABLE tratamiento (
   paciente_cedula INT NOT NULL,
   medico_cedula INT NOT NULL,
   PRIMARY KEY (idTratamiento),
-  INDEX fk_tratamiento_paciente_idx (paciente_cedula ASC) VISIBLE,
-  INDEX fk_tratamiento_medico1_idx (medico_cedula ASC) VISIBLE,
   CONSTRAINT fk_tratamiento_paciente
     FOREIGN KEY (paciente_cedula)
-    REFERENCES paciente (cedula)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES paciente (cedula),
   CONSTRAINT fk_tratamiento_medico1
     FOREIGN KEY (medico_cedula)
-    REFERENCES medico (cedula)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES medico (cedula))
+
 
 
 CREATE TABLE rehabilitacion (
@@ -59,7 +53,7 @@ CREATE TABLE rehabilitacion (
   fechaFin VARCHAR(45) NULL,
   descripcion VARCHAR(45) NULL,
   PRIMARY KEY (idRehabilitacion))
-ENGINE = InnoDB;
+
 
 
 CREATE TABLE cita (
@@ -69,40 +63,28 @@ CREATE TABLE cita (
   paciente_cedula INT NOT NULL,
   medico_cedula INT NOT NULL,
   PRIMARY KEY (idCita),
-  INDEX fk_cita_paciente1_idx (paciente_cedula ASC) VISIBLE,
-  INDEX fk_cita_medico1_idx (medico_cedula ASC) VISIBLE,
   CONSTRAINT fk_cita_paciente1
     FOREIGN KEY (paciente_cedula)
-    REFERENCES paciente (cedula)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES paciente (cedula),
   CONSTRAINT fk_cita_medico1
     FOREIGN KEY (medico_cedula)
-    REFERENCES medico (cedula)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES medico (cedula))
+
 
 
 CREATE TABLE TxR (
-  rehabilitacion_idRehabilitacion INT NOT NULL,
-  tratamiento_idTratamiento INT NOT NULL,
-  INDEX fk_TxR_rehabilitacion1_idx (rehabilitacion_idRehabilitacion ASC) VISIBLE,
-  INDEX fk_TxR_tratamiento1_idx (tratamiento_idTratamiento ASC) VISIBLE,
-  CONSTRAINT fk_TxR_rehabilitacion1
-    FOREIGN KEY (rehabilitacion_idRehabilitacion)
-    REFERENCES rehabilitacion (idRehabilitacion)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_TxR_tratamiento1
-    FOREIGN KEY (tratamiento_idTratamiento)
-    REFERENCES tratamiento (`idTratamiento`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  rehab_idRehabilitacion INT NOT NULL,
+  trat_idTratamiento INT NOT NULL,
+  CONSTRAINT fk_rehabilitacion1
+    FOREIGN KEY (rehab_idRehabilitacion)
+    REFERENCES rehabilitacion (idRehabilitacion),
+  CONSTRAINT fk_tratamiento1
+    FOREIGN KEY (trat_idTratamiento)
+    REFERENCES tratamiento (idTratamiento))
 
 
-CREATE TABLE IF NOT EXISTS TxM (
+
+CREATE TABLE  TxM (
   medicamento_nombre VARCHAR(100) NOT NULL,
   tratamiento_idTratamiento INT NOT NULL,
   dosis VARCHAR(45) NULL,
@@ -110,21 +92,9 @@ CREATE TABLE IF NOT EXISTS TxM (
   fechaFin VARCHAR(45) NULL,
   frecuencia VARCHAR(45) NULL,
   horasTomas VARCHAR(200) NULL,
-  INDEX fk_TxM_medicamento1_idx (medicamento_nombre ASC) VISIBLE,
-  INDEX fk_TxM_tratamiento1_idx (tratamiento_idTratamiento ASC) VISIBLE,
   CONSTRAINT fk_TxM_medicamento1
     FOREIGN KEY (medicamento_nombre)
-    REFERENCES medicamento (nombre)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES medicamento (nombre),
   CONSTRAINT fk_TxM_tratamiento1
     FOREIGN KEY (tratamiento_idTratamiento)
-    REFERENCES tratamiento (idTratamiento)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+    REFERENCES tratamiento (idTratamiento))
